58f36b49f4ac390e970f1379bad80de864cb6b25
omontoy <omb0320@gmail.com>
2019-11-19 13:42:41 -0500

    Modificación PostgreSQL

--- app/controllers/constructions_controller.rb
+++ app/controllers/constructions_controller.rb
@@ -9,7 +9,7 @@ class ConstructionsController < ApplicationController
       @constructions = Construction.order('created_at ASC')
 
       if params[:name].present? 
-        @constructions = @constructions.where("name ILIKE ?", "%#{params[:name].strip}%")
+        @constructions = @constructions.where("code ILIKE ?", "%#{params[:name].strip}%")
       end
    end
 
